0d0209ac6826b41ed7413546dfa1c542580d48ed
Andrzej Fricze <a.fricze@pm.me>
2022-11-29 15:12:27 +0100

    Create redux todo

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import { render } from 'react-dom'
 import 'antd/dist/antd.css'
 import './styles.css'
 import App from './App'
+import AppRedux from './AppRedux'
 
-render(<App />, document.getElementById('root'))
+render(<AppRedux />, document.getElementById('root'))
